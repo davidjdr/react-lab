a50c84914820dd5a4636017618024f03ab156b82
David De Freitas <david.defreitasrosales@cencosud.cl>
2019-02-11 12:28:56 -0300

    Estado de los componentes en ReactJs

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -5,18 +5,81 @@ import './media.css';
 class Media extends Component {
     
     //forma de hacerlo con ES6
-    /*
-    constructor(props) {
+    
+   /* constructor(props) {
         super(props)
         //enlazo evento con clase, se cambia el contexto de la función
-        this.handleClick = this.handleClick.bind(this);
+        //this.handleClick = this.handleClick.bind(this);
+
+        //Esta es la manera anterior para trabajar con ES menor a 7
+        //Estado de los componentes en ReactJs
+        this.state = {
+            author: props.author
+        }
+
+        //0 Constructor: método llamado antes de que el componente sea 
+        //montado (el componente aún no se ve)
+        //Dentro del ciclo de vida de un componente esto representa:
+        // Enlazo (bind) eventos y/o inicializo estado
+    }*/
+    
+    //ES7 de manejar estado de los componentes
+    state = {
+        author: 'David Jesus'
     }
-    */
+
+    componentWillMount(){
+        //1 Se ejecuta antes de montar el componente
+        // Se podría usar para hacer un setState()
+    }
+
+    render(){
+        //2 Contiene todos los elementos a renderizar
+        // podrías usarlo para calcular propiedades (ej: concatenar una cadena)
+    }
+
+    componentDidMount(){
+        //3 Solo se lanza una vez
+        //Ideal para llamar a una API, hacer un setInteval, etc
+    }
+
+    //Actualización:
+    componentWillReceiveProps(){
+        //4 Es llamado cuando el componente recibe nuevas propiedades.
+
+    }
+
+    shouldComponentUpdate(){
+        //5 Idea para poner una condición y  si las propiedades que le llegaron anteriormente
+        // eran las mismas que tenia retornar false para evitar re-renderear el componente
+    }
+
+    componentWillUpdate(){
+        //6 metodo llamado antes de re-renderizar el componente si shouldComponentUpdate devolvió true
+    }
+
+    // re-render si es necesario...
+    componentDidUpdate(){
+        //7 Método llamado luego del re-render
+    }
+
+    componentWillUnmount(){
+        //8 Método llamado antes de desmontar el componente
+    }
+
+    componentDidCatch(){
+        //9 Si ocurre algún error, lo capturo desde acá:
+    }
+
 
     //gracias al uso de arrow function (que heredan el contexto del padre)
     //puedo usar el this.props, sin necesidad de hacer el bind en el constructor
     handleClick = (event) => {
-        console.log(this.props.image);
+       //console.log(this.props.image);
+       //llamamos a un metodo del componente para cambiar el estado
+       this.setState({
+           author: 'David Rosales'
+       })
     }
 
     render(){
@@ -41,7 +104,7 @@ class Media extends Component {
                    />
                 </div>
                 <h3 className="Media-title">{this.props.title}</h3>
-                <p className="Media-author">{this.props.author}</p>
+                <p className="Media-author">{this.state.author}</p>
             </div>
         )
     }
@@ -63,3 +126,7 @@ Media.propTypes = {
 //Doc de PropType: https://reactjs.org/docs/typechecking-with-proptypes.html
 
 export default Media;
+
+/*
+Ciclo de vida de los componentes
+*/
